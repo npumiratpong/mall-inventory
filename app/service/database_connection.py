b2d78e039f62f0124aad085430f65c35d6285e19
sromlee <suleeporn.romlee@gmail.com>
2023-02-01 22:41:52 +0700

    add condition to check if result exists in get_items()

--- app/service/database_connection.py
+++ app/service/database_connection.py
@@ -2,7 +2,7 @@ from sqlalchemy.orm import Session
 from sqlalchemy.sql import text
 from fastapi import Request
 from typing import Dict, List, Optional, Union
-    
+
 from ..models import models
 from ..models import schemas
 
@@ -14,6 +14,7 @@ def create_user_item(db: Session, item: schemas.ItemCreate, user_id: int):
     db.refresh(db_item)
     return db_item
 
+
 def get_user_by_username(db: Session, username: str) -> Dict:
     sql = text(f"SELECT u.user_id, u.username, u.hashed_password,r.role_name, u.is_active FROM users u " \
                f"INNER JOIN user_mapping um " \
@@ -23,20 +24,21 @@ def get_user_by_username(db: Session, username: str) -> Dict:
                f"WHERE u.username = '{username}'")
     result = db.execute(sql).fetchall()[0]
     user = {
-            "user_id": result[0],
-            "username": result[1],
-            "hashed_password": result[2],
-            "role": result[3],  
-            "is_active": result[4]
-            }
-    return schemas.User(**user)  
+        "user_id": result[0],
+        "username": result[1],
+        "hashed_password": result[2],
+        "role": result[3],
+        "is_active": result[4]
+    }
+    return schemas.User(**user)
 
-def get_items(db: Session, type:str) -> List:
+
+def get_items(db: Session, type: str) -> List:
     sql = text(f"SELECT {type} FROM Products")
     result = db.execute(sql).fetchall()
-    if len(result) == 1:
-        items = str(result[0]).strip("()',")
-    else:
-        items: list = [str(x).strip("()',") for x in result]
+    if result:
+        if len(result) == 1:
+            items = str(result[0]).strip("()',")
+        else:
+            items: list = [str(x).strip("()',") for x in result]
     return items
-        
