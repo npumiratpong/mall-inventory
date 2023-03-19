582c1ae4a4f87d112f9630b2d135e9c05c1b72ba
npumiratpong <rinpmp@gmail.com>
2023-03-19 16:19:51 +0700

    update linux

--- linux/setup_crons.sh
+++ linux/setup_crons.sh
@@ -6,7 +6,7 @@ PROCESS_CHECKS="/root/process_checks.sh"
 JOB_SCHEDULER="/root/job_runner.sh"
 
 # Set up the cronjob
-(crontab -l 2>/dev/null; echo "*/1 * * * * $$PROCESS_CHECKS") | crontab -
+(crontab -l 2>/dev/null; echo "*/1 * * * * $PROCESS_CHECKS") | crontab -
 (crontab -l 2>/dev/null; echo "05 * 05 * * $JOB_SCHEDULER") | crontab -
 
 echo "Cronjob set up to run $PYTHON_SCRIPT_PATH every minute."
